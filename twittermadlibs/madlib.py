73c8ca6ddd898eaeece320ed0cf0fbb3df483b9b
Charlie <charles.w.marlow@gmail.com>
2019-02-09 16:03:01 -0500

    test

---
+++ twittermadlibs/madlib.py
@@ -0,0 +1,20 @@
+import csv
+
+class madlib:
+
+    def __init__(self, partsOfSpeech, madlibFile):
+        self.wordsDict = partsOfSpeech
+        self.file = madLibFile
+
+    def getMadlib(self):
+
+    def createMadlib(self):
+
+    def readCSV(self):
+
+
+
+# read CSV as a list
+# choose list.random() to getMadlib
+# for each word in string, if word[0] = # then parse it
+# return the madLib as a string
