6afb6b8209fbaf5fa83f94453a05bc3db27592ff
Charlie <charles.w.marlow@gmail.com>
2019-02-09 18:05:20 -0500

    fixed indentation errors

--- twittermadlibs/__main__.py
+++ twittermadlibs/__main__.py
@@ -9,8 +9,7 @@ madlibFile = "/../Madlibs_Templates/test.csv"
 
 def main():
     print("in main")
-
-	# get username
+    # get username
     '''
     print("Enter a twitter username, no @")
     username = input()
@@ -18,14 +17,14 @@ def main():
     '''
 
     # pass username to getTweets, return a list of words
-	twitterInterface = pullTweets("realDonaldTrump")
-	tweets = twitterInterface.get_tweets()
-    
+    twitterInterface = pullTweets("realDonaldTrump")
+    tweets = twitterInterface.get_tweets()
+
     # unique takes TextBlob object as a param, returns new textblob
-	uniqueParser = unique(tweets)
-	uniqueStr = uniqueParser.getTweetList()
-	print(uniqueStr)
-	unique2 = " ".join(uniqueStr)
+    uniqueParser = unique(tweets)
+    uniqueStr = uniqueParser.getTweetList()
+    print(uniqueStr)
+    unique2 = " ".join(uniqueStr)
     # pass refinedList to parser to get a dictionary
     tweetBlob = TextBlob(unique2)
     classifier = classifyWords(tweetBlob)
@@ -35,6 +34,7 @@ def main():
     # pass the dictionary to create madlibs, return a string
     madlibGenerator = madlib(tweetDict, madlibFile)
     madlibStr = madlibGenerator.createMadlib()
+    print(madlibStr)
     # pass a string to the formatter, return an HTMLString
     # print the string
 
