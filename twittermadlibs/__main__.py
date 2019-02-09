599aebd8e9269a22c0a6491642d2fb5dcdd586c6
Charlie <charles.w.marlow@gmail.com>
2019-02-09 14:03:45 -0500

    edited main

--- twittermadlibs/__main__.py
+++ twittermadlibs/__main__.py
@@ -1,22 +1,32 @@
 import sys
-from .classifyWords import classifyWords
+from .classifyWords import classifyWord
+from .pullTweets import pullTweets
 from textblob import TextBlob
 
 def main():
     print("in main")
 
     # get username
+    '''
     print("Enter a twitter username, no @")
     username = input()
+    print("username is " + username)
 
-    
     # pass username to getTweets, return a list of words
+    twitterInterface = pullTweets(username)
+    tweets = twitterInterface.get_tweets()
+    print(tweets)
+    '''
+
     # pass list of words to unique, get refinedList
-    test = TextBlob("Wow. Um. Ok? You couldn't have more wholesome more say to way to say that? Bad words have no more common to make you funny! STOP curse words, block your F word or im downvoting! Cursing makes the bottom of the line: the lowest of class! People like you act like sin is all fun an game, but all they do is make me more mad!")
-    classifier = classifyWords(test)
-    test2 = classifier.createWordDictionary()
-    print(test2)
+
     # pass refinedList to parser to get a dictionary
+    tweetBlob = TextBlob(tweets)
+    classifier = classifyWords(tweetBlob)
+    tweetDict = classifier.createWordDictionary()
+    print(tweetDict)
+
+
     # pass the dictionary to create madlibs, return a string
     # pass a string to the formatter, return an HTMLString
     # print the string
